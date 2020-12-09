ec5d6a96ee0dede9132a18e5d94f6255c5868453
bayramkarahan <44923603+bayramkarahan@users.noreply.github.com>
2020-12-09 18:24:25 +0300

    Update remove.sh

--- remove.sh
+++ remove.sh
@@ -10,24 +10,11 @@ if [ $ver == $version ]
 then
 #**********************************************
 echo "üç"
-rm /usr/bin/E-Tahta
-rm $FILE
-rm /usr/bin/E-Tahta.desktop
-rm /usr/bin/E-Tahta.ico
-rm /usr/bin/E-Tahtaxdglink.sh
-rm /etc/xdg/autostart/E-Tahtaetcxdgautostart.desktop
-echo "Dosya Silindi.."    
+
 #************************************************
 else
 #**********************************************
-rm /usr/bin/E-Tahta
-rm $FILE
-rm /usr/bin/E-Tahta.desktop
-rm /usr/share/applications/E-Tahta.desktop
-rm /usr/bin/E-Tahta.png
-rm /usr/bin/E-Tahtaxdglink.sh
-rm /etc/xdg/autostart/E-Tahtaetcxdgautostart.desktop
-echo "Dosya Silindi.."    
+
 #*****************************************
 fi
 
